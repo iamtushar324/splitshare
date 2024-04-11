12d7e8dbbef0469e5f97c89863d5104450cc78ac
tushar bhardwaj <iamtushar324@gmail.com>
2024-04-11 20:55:27 +0530

    Added boilerplate for gin server with redis and postgres setup in docker
    Signed-off-by: tushar bhardwaj <iamtushar324@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+## Installation 
+
+```
+go install 
+```
+
+```
+$ docker-compose up -d
+```
+
+Import postgres database 
+
+```
+$ psql -U postgres -h localhost < ./server/db/database.sql
+```
+
--- server/main.go
+++ server/main.go
@@ -1,41 +1,112 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
+	"os"
+
+	"github.com/gin-contrib/gzip"
+	uuid "github.com/google/uuid"
+	"github.com/iamtushar324/splitshare/server/controllers"
+	"github.com/iamtushar324/splitshare/server/db"
+	"github.com/iamtushar324/splitshare/server/forms"
+	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
+	"github.com/gin-gonic/gin/binding"
 )
 
+//CORSMiddleware ...
+//CORS (Cross-Origin Resource Sharing)
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
+
+//RequestIDMiddleware ...
+//Generate a unique ID and attach it to each request for future reference or use
+func RequestIDMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		uuid := uuid.New()
+		c.Writer.Header().Set("X-Request-Id", uuid.String())
+		c.Next()
+	}
+}
+
+var auth = new(controllers.AuthController)
+
+//TokenAuthMiddleware ...
+//JWT Authentication middleware attached to each request that needs to be authenitcated to validate the access_token in the header
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		auth.TokenValid(c)
+		c.Next()
+	}
+}
+
 func main() {
-	// Initialize database connection
-	db, err := sql.Open("postgres", "postgres://postgres:password@postgres:5432/mydatabase?sslmode=disable")
+	//Load the .env file
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error opening database connection: %v", err)
-	}
-	defer db.Close()
-
-	// Test database connection
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
+		log.Fatal("error: failed to load the env file")
 	}
 
+	if os.Getenv("ENV") == "PRODUCTION" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	//Start the default gin server
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to my Go server!",
-		})
-	})
+	//Custom form validator
+	binding.Validator = new(forms.DefaultValidator)
 
-	// Define your routes and validation logic here
+	r.Use(CORSMiddleware())
+	r.Use(RequestIDMiddleware())
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
 
-	// Start server
-	port := ":8080"
-	fmt.Printf("Server listening on port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, r))
+	//Start PostgreSQL database
+	//Example: db.GetDB() - More info in the models folder
+	db.Init()
+
+	//Start Redis on database 1 - it's used to store the JWT but you can use it for anythig else
+	//Example: db.GetRedis().Set(KEY, VALUE, at.Sub(now)).Err()
+	db.InitRedis(1)
+
+	v1 := r.Group("/v1")
+	{
+		/*** START USER ***/
+		user := new(controllers.UserController)
+
+		v1.POST("/user/login", user.Login)
+		v1.POST("/user/register", user.Register)
+		v1.GET("/user/logout", user.Logout)
+
+		/*** START AUTH ***/
+		auth := new(controllers.AuthController)
+
+		//Refresh the token when needed to generate new access_token and refresh_token for the user
+		v1.POST("/token/refresh", auth.Refresh)
+
+	}
+
+	port := os.Getenv("PORT")
+
+	log.Printf("\n\n PORT: %s \n ENV: %s \n SSL: %s \n Version: %s \n\n", port, os.Getenv("ENV"), os.Getenv("SSL"), os.Getenv("API_VERSION"))
+
+	r.Run(":" + port)
+
 }
